f78ff3c7572f4f98304b0a97f2f74b1543550f8a
Ayo-mi <ajayijubrilayo@gmail.com>
2023-04-22 12:46:43 +0100

    New Solutions

--- CodingTest/GenerateParenthesis.cs
+++ CodingTest/GenerateParenthesis.cs
@@ -8,5 +8,25 @@ namespace CodingTst
 {
     internal class GenerateParenthesis
     {
+        public IList<string> GenerateParentheses(int n)
+        {
+            List<string> ans = new List<string>();
+            if (n == 0)
+                ans.Add("");
+            else
+            {
+                for (int c = 0; c < n; ++c)
+                {
+                    foreach (var left in GenerateParentheses(c))
+                    {
+                        foreach (var right in GenerateParentheses(c))
+                        {
+                            ans.Add($"({left}){right}");
+                        }
+                    }
+                }
+            }
+            return ans;
+        }
     }
 }
